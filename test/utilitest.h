e0bdafa9ac8058d9558a99cbde840e65d1561afc
Trey Amador <tmamador@cpp.edu>
2018-02-15 02:22:44 -0800

    testing framework update

--- test/utilitest.h
+++ test/utilitest.h
@@ -1,9 +1,12 @@
 #ifndef UTILITEST_H_
 #define UTILITEST_H_
 #include <iostream>
+#include <iterator>
+#include <stdlib.h>
 #include <iomanip>
 #include <utility>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <map>
@@ -17,11 +20,11 @@ namespace {
 }
 
 
-typedef std::pair<mtx::Matrix<double>*,mtx::Matrix<double>*> MatrixPair;
-typedef std::vector<MatrixPair> MatrixPairs;
+typedef std::vector<std::pair<mtx::Matrix<double>,mtx::Matrix<double> > > MatrixPairs;
 
 
 struct test {
+
 
     private:
         template<typename T>
@@ -31,10 +34,12 @@ struct test {
             return false;
         }
 
+
         template<typename T>
         static bool comparable(T& res, T& ans) {
             return equivalent(res,ans);
         }
+
 
         template<typename T>
         static bool comparable(mtx::Matrix<T>& res, mtx::Matrix<T>& ans) {
@@ -48,10 +53,12 @@ struct test {
             return true;
         }
 
+
         template<typename T>
         static void console(T& a, T& b) {
             std::cout << "\n" << a << " " << b << "\n" << std::endl;
         }
+
 
         template<typename T>
         static void outline(T& a) {
@@ -64,6 +71,7 @@ struct test {
                 std::left <<
                 a << " ";
         }
+
 
         template<typename T>
         static void console(mtx::Matrix<T>& res, mtx::Matrix<T>& ans) {
@@ -80,14 +88,28 @@ struct test {
             std::cout << std::endl;
         }
 
+
         static void successful() {
             std::cout << "\n" <<
                 "Inputs are similar" << "\n\n";
         }
 
+
         static void unsuccessful() {
             std::cout << "\n"
                 "Inputs are not similar" << "\n\n";
+        }
+
+
+
+    public:
+        static std::vector<std::string> split(const std::string& s, char delimiter) {
+           std::vector<std::string> tokens;
+           std::string token;
+           std::istringstream tokenStream(s);
+           while (std::getline(tokenStream, token, delimiter))
+              tokens.push_back(token);
+           return tokens;
         }
 
 
@@ -106,121 +128,26 @@ struct test {
         }
 
 
-        /*
-
-        static std::vector<std::pair<mtx::Matrix<double>,mtx::Matrix<double> > >
-            processMatrices(std::vector<std::string>& lines)
-        {
-            auto iter = lines.begin();
-            std::vector<std::pair<mtx::Matrix<double>,mtx::Matrix<double> > > cmp;
-            while (iter != lines.end()) {
-                mtx::Matrix<double> q = processMatrix(*iter);
-                ++iter;
-                mtx::Matrix<double> a = processMatrix(*iter);
-                ++iter;
-                cmp.push_back(std::make_pair(q,a));
-
-                std::pair<mtx::Matrix
-
-            }
-            return cmp;
-        }
-
-        */
-
-
-
-        /*
-        static std::vector<std::map<char,mtx::Matrix<double> > >
-            processMatrices(std::vector<std::string>& lines)
-        {
-            auto iter = lines.begin();
-            std::vector<std::map<char,mtx::Matrix<double> > > cmp;
-            while (iter != lines.end()) {
-
-                mtx::Matrix<double> q = processMatrix(*iter);
-                ++iter;
-                mtx::Matrix<double> a = processMatrix(*iter);
-                ++iter;
-                std::map<char,mtx::Matrix<double> > elem = {
-                    {'q',q},
-                    {'a',a}
-                };
-                cmp.push_back(elem);
-            }
-
-            return cmp;
-        }
-        */
-
-        /*
-
-        static MatrixPairs processMatrices(
-            std::vector<std::string>& lines)
-        {
-
-            //std::vector<mtx::Matrix<double>* > matrices;
-
-
-            MatrixPairs matrices;
-
-
-            for (int i = 0; i < lines.size(); i += 2) {
-
-                mtx::Matrix<double> Q(0,0);
-                mtx::Matrix<double> A(0,0);
-
-                processMatrix(lines[i],Q);
-                processMatrix(lines[i+1],A);
-
-                Q.print();
-
-                matrices.push_back(
-                    std::make_pair(&Q,&A)
-                );
-
-            }
-
-            return matrices;
-        }
-
-        */
-
-
         static mtx::Matrix<double> processMatrix(std::string& str) {
-            int len = str.size(),index = 6,
-                r = 2,c = 4,m = 0;
+            int r = 2,c = 4;
             int rows = str[r]-'0',
                 cols = str[c]-'0';
             mtx::Matrix<double> matrix(rows,cols);
-            for (int i = index; i < len; ++i) {
-                int sum = 0;
-                char ch = str[i];
-                while (ch != ',') {
-                    sum = sum*10 + ch-'0';
-                    ch = str[++i];
-                }
-                matrix[m++] = sum;
-            }
+            std::vector<std::string> elements = split(str,',');
+            elements.erase(elements.begin());
+            for (int i = 0; i < elements.size(); ++i)
+                matrix[i] = atof(elements[i].c_str());
             return matrix;
         }
 
 
-        static std::vector<std::pair<mtx::Matrix<double>,mtx::Matrix<double> > >
-            procesMatrices(std::string filepath)
-        {
-
-            std::vector<std::pair<mtx::Matrix<double>,mtx::Matrix<double> > > res;
+        static MatrixPairs procesMatrices(std::string filepath) {
             std::vector<std::string> lines = readlines(filepath);
-            for (int i = 0; i < lines.size(); i += 2) {
-
-                mtx::Matrix<double> q = processMatrix(lines[i]);
-                mtx::Matrix<double> a = processMatrix(lines[i+1]);
-                res.push_back(std::make_pair(q,a));
-
-
-            }
-
+            MatrixPairs res;
+            for (int i = 0; i < lines.size(); i += 2)
+                res.push_back(std::make_pair(
+                    processMatrix(lines[i]),
+                    processMatrix(lines[i+1])));
             return res;
         }
 
