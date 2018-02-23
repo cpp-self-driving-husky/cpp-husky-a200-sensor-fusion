1937cd2444e633ce8f3d7dd1dcc13626ab13589a
Trey Amador <tmamador@cpp.edu>
2018-02-23 00:34:37 -0800

    update calc and transpose

--- src/calculator.h
+++ src/calculator.h
@@ -9,67 +9,45 @@
 #include<iostream>
 
 
-
-namespace {
-    const int DEFAULT_STORAGE = 100;
-};
-
-
 namespace calc {
 
     template<class T>
     class Calculator {
 
         public:
-            Calculator() :
-                square_(mtx::Matrix<T>(DEFAULT_STORAGE,DEFAULT_STORAGE)),
+            Calculator(int vars, int meas) :
+                trans_store_(mtx::Matrix<T>(vars,meas)),
+                inv_store_(mtx::Matrix<T>(meas,meas)),
                 state_x_(nullptr),
                 state_y_(nullptr),
                 indeces_(nullptr),
                 vars_(0),
-                storage_(nullptr)
+                meas_(0)
             {
-                this->init(DEFAULT_STORAGE);
-            }
-
-            Calculator(int elems) :
-                square_(mtx::Matrix<T>(elems,elems)),
-                state_x_(nullptr),
-                state_y_(nullptr),
-                indeces_(nullptr),
-                vars_(0),
-                storage_(nullptr)
-            {
-                this->init(elems);
+                this->init(vars,meas);
             }
 
             ~Calculator() {
                 this->destroy();
             }
 
-            void init(int elems) {
+            void init(int vars,int meas) {
                 this->destroy();
-                this->vars_ = elems;
-                this->square_.init(elems,elems);
-                this->state_x_ = new T[this->vars_];
-                this->state_y_ = new T[this->vars_];
-                this->indeces_ = new int[this->vars_];
-                this->storage_ = new T[this->storageSize(this->vars_)];
-            }
-
-            int storageSize(int elems) {
-                int storage_size = static_cast<int>(std::pow(elems,3));
-                return
-                    storage_size > DEFAULT_STORAGE ?
-                    storage_size : DEFAULT_STORAGE;
+                this->vars_ = vars;
+                this->meas_ = meas;
+                this->trans_store_.init(vars,meas);
+                this->inv_store_.init(meas,meas);
+                this->state_x_ = new T[this->meas_];
+                this->state_y_ = new T[this->meas_];
+                this->indeces_ = new int[this->meas_];
             }
 
             void destroy() {
-                this->square_.destroy();
+                this->trans_store_.destroy();
+                this->inv_store_.destroy();
                 this->deallocate(this->state_x_);
                 this->deallocate(this->state_y_);
                 this->deallocate(this->indeces_);
-                this->deallocate(this->storage_);
                 this->vars_ = 0;
             }
 
@@ -79,12 +57,6 @@ namespace calc {
                     delete[] elements;
                     elements = nullptr;
                 }
-            }
-
-            void store(mtx::Matrix<T>& matrix) {
-                int elems = matrix.getSize();
-                for (int i = 0; i < elems; ++i)
-                    this->square_[i] = matrix[i];
             }
 
             template<typename S>
@@ -133,11 +105,6 @@ namespace calc {
                 }
             }
 
-            void transpose(mtx::Matrix<T>& trans) {
-                this->store(trans);
-                this->transpose(trans,this->square_);
-            }
-
             void transpose(mtx::Matrix<T>& trans, mtx::Matrix<T>& matrix) {
                 int rows = trans.getRows(),
                     cols = trans.getCols();
@@ -168,23 +135,17 @@ namespace calc {
                 P.zero();
                 int rows = A.getRows(),
                     inner = A.getCols(),
-                    cols = B.getCols();
+                    cols = B.getRows();
                 for (int i = 0; i < rows; ++i)
                     for (int j = 0; j < cols; ++j)
                         for (int k = 0; k < inner; ++k)
-                            P[i*cols+j] += A[i*inner+k] * B[j*cols+k];
+                            P[i*cols+j] += A[i*inner+k] * B[j*inner+k];
             }
 
             void multiplyABAt(mtx::Matrix<T>& P,mtx::Matrix<T>& A,mtx::Matrix<T>& B) {
-                this->multiply(this->square_,A,B);
-                this->multiplyABt(P,this->square_,A);
+                this->multiply(this->trans_store_,A,B);
+                this->multiplyABt(P,this->trans_store_,A);
             }
-
-            void cholesky(mtx::Matrix<T>& L) {
-                this->store(L);
-                this->cholesky(L,this->square_);
-            }
-
 
             // TODO there is some rounding error
             //      which causes problems in calculation!
@@ -239,13 +200,13 @@ namespace calc {
                 int len = LU.getRows();
                 for (int i = 0; i < len; ++i) {
                     for (int j = 0; j < len; ++j)
-                        this->square_[i*len+j] = 0.0;
-                    this->square_[i*len+i] = 1.0;
+                        this->inv_store_[i*len+j] = 0.0;
+                    this->inv_store_[i*len+i] = 1.0;
                     for (int n = 0; n < len; ++n) {
                         T t = 0.0;
                         for (int m = 0; m <= n-1; ++m )
                             t += LU[n*len+m]*this->state_y_[m];
-                        this->state_y_[n] = this->square_[i*len+this->indeces_[n]]-t;
+                        this->state_y_[n] = this->inv_store_[i*len+this->indeces_[n]]-t;
                     }
                     for (int n = len-1; n>=0; --n) {
                         T t = 0.0;
@@ -254,39 +215,31 @@ namespace calc {
                         this->state_x_[n] = (this->state_y_[n]-t) / LU[n*len+n];
                     }
                     for (int j = 0; j < len; ++j)
-                        this->square_[i*len+j] = this->state_x_[j];
+                        this->inv_store_[i*len+j] = this->state_x_[j];
                 }
                 for (int i = 0; i < len; ++i)
                     for (int j = 0; j < len; ++j)
-                        LU[i*len+j] = this->square_[j*len+i];
+                        LU[i*len+j] = this->inv_store_[j*len+i];
                 return true;
             }
 
-            // TODO actually test if invertible
-            void inverse(mtx::Matrix<T>& I) {
-                this-LUPdecompose(I);
-                this->LUPinverse(I);
-            }
-
-            // TODO actually test if invertible
             void inverse(mtx::Matrix<T>& I, mtx::Matrix<T>& M) {
                 I = M;
                 this->LUPdecompose(I);
                 this->LUPinverse(I);
             }
 
-            int getStorageSize() {
-                return this->square_.getSize();
-            }
-
 
         private:
-            mtx::Matrix<T> square_;
+            mtx::Matrix<T> trans_store_;
+            mtx::Matrix<T> inv_store_;
+
             int* indeces_;
             T* state_x_;
             T* state_y_;
+
             int vars_;
-            T* storage_;
+            int meas_;
 
     };
 
--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,85 @@
 
 
 
+void testCalculatorA() {
+
+    int row = 3, col = 4;
+    int inner = col;
+    calc::Calculator<double> calc(row,col);
+
+    mtx::Matrix<double> A(row,inner);
+    mtx::Matrix<double> B(inner,col);
+    mtx::Matrix<double> P(row,row);
+
+    std::vector<double> a = {
+        6,5,7,3,
+        7,3,9,8,
+        2,5,1,4
+    };
+    std::vector<double> b = {
+        1,7,4,9,
+        6,5,1,6,
+        7,8,4,1,
+        1,2,7,4
+    };
+
+
+    for (int i = 0; i < A.getSize(); ++i)
+        A[i] = a[i];
+    for (int i = 0; i < B.getSize(); ++i)
+        B[i] = b[i];
+
+
+    calc.multiplyABAt(P,A,B);
+
+    A.precisionPrint();
+    B.precisionPrint();
+    P.precisionPrint();
+
+}
+
+
+
+void testCalculatorB() {
+
+    int row = 5, col = 3;
+    int inner = col;
+    calc::Calculator<double> calc(row,col);
+
+    mtx::Matrix<double> A(row,inner);
+    mtx::Matrix<double> B(inner,col);
+    mtx::Matrix<double> P(row,row);
+
+    std::vector<double> a = {
+        6,5,7,
+        3,7,3,
+        9,8,2,
+        5,1,4,
+        7,2,3
+    };
+    std::vector<double> b = {
+        1,7,4,
+        9,6,5,
+        1,6,7,
+        8,4,1,
+        1,2,7,4
+    };
+
+
+    for (int i = 0; i < A.getSize(); ++i)
+        A[i] = a[i];
+    for (int i = 0; i < B.getSize(); ++i)
+        B[i] = b[i];
+
+
+    calc.multiplyABAt(P,A,B);
+
+    A.precisionPrint();
+    B.precisionPrint();
+    P.precisionPrint();
+
+}
+
 
 
 
@@ -16,7 +95,7 @@ void testCovariance() {
         int cols = matrices[i].first.getCols();
         state::StateVector<double> mean(cols);
         mtx::Matrix<double> cov(cols,cols);
-        calc::Calculator<double> calc(cols);
+        calc::Calculator<double> calc(cols,cols);
 
         calc.mean(mean,matrices[i].first);
         calc.covariance(cov,matrices[i].first,mean);
@@ -38,7 +117,7 @@ void testCholesky() {
         state::StateVector<double> mean(cols);
         mtx::Matrix<double> cov(cols,cols);
         mtx::Matrix<double> chol(cols,cols);
-        calc::Calculator<double> calc(cols);
+        calc::Calculator<double> calc(cols,cols);
 
         calc.mean(mean,matrices[i].first);
         calc.covariance(cov,matrices[i].first,mean);
@@ -59,7 +138,7 @@ void testInverse() {
 
         int cols = matrices[i].first.getCols();
         mtx::Matrix<double> inv(cols,cols);
-        calc::Calculator<double> calc(cols);
+        calc::Calculator<double> calc(cols,cols);
         calc.inverse(inv,matrices[i].first);
 
         test::run(inv,matrices[i].second);
@@ -69,15 +148,19 @@ void testInverse() {
 
 
 
-
-
-
 int main(int argc, char* argv[]) {
 
     testCovariance();
     testCholesky();
     testInverse();
 
+    testCalculatorA();
+    testCalculatorB();
+
     return 0;
 
 }
+
+
+
+
