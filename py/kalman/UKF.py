a7df8561c01a2613fde3a5dcdc21c05ac3046cee
Trey Amador <tmamador@cpp.edu>
2018-04-19 17:55:36 -0700

    py refactor unnecessary fxns

--- py/kalman/UKF.py
+++ py/kalman/UKF.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 from __future__ import (absolute_import, division)
 
 import math
@@ -12,57 +13,13 @@ from kalman.stats import logpdf
 
 def unscented_transform(sigmas, Wm, Wc, noise_cov=None,
                         mean_fn=None, residual_fn=None):
-    """
-    Attributes
-    ----------
-    
-    sigmas: ndarray [#sigmas per dimension, dimension]
-        2D array of sigma points.
-
-    Wm : ndarray [# sigmas per dimension]
-        Weights for the mean.
-
-    Wc : ndarray [# sigmas per dimension]
-        Weights for the covariance.
-
-    noise_cov : ndarray, optional
-        noise matrix added to the final computed covariance matrix.
-
-    mean_fn : callable (sigma_points, weights), optional
-        Function that computes the mean of the provided sigma points
-        and weights. Use this if your state variable contains nonlinear
-        values such as angles which cannot be summed.
-
-    residual_fn : callable (x, y), optional
-
-        Function that computes the residual (difference) between x and y.
-        You will have to supply this if your state variable cannot support
-        subtraction, such as angles (359-1 degreees is 2, not 358). x and y
-        are state vectors, not scalars.
-
-    Returns
-    -------
-
-    x : ndarray [dimension]
-        Mean of the sigma points after passing through the transform.
-
-    P : ndarray
-        covariance of the sigma points after passing throgh the transform.
-
-    """
 
     kmax, n = sigmas.shape
-
     if mean_fn is None:
-        # new mean is just the sum of the sigmas * weight
-        x = np.dot(Wm, sigmas)    # dot = \Sigma^n_1 (W[k]*Xi[k])
+        x = np.dot(Wm, sigmas)
     else:
         x = mean_fn(sigmas, Wm)
 
-    # new covariance is the sum of the outer product of the residuals
-    # times the weights
-
-    # this is the fast way to do this - see 'else' for the slow way
     if residual_fn is np.subtract or residual_fn is None:
         y = sigmas - x[np.newaxis,:]
         P = np.dot(y.T, np.dot(np.diag(Wc), y))
@@ -78,112 +35,13 @@ def unscented_transform(sigmas, Wm, Wc, noise_cov=None,
     return (x, P)
 
 
-class UnscentedKalmanFilter(object):
-    r""" 
-    Attributes
-    ----------
+class UnscentedKalmanFilter:
 
-    x : numpy.array(dim_x)
-        state estimate vector
-
-    P : numpy.array(dim_x, dim_x)
-        covariance estimate matrix
-
-    R : numpy.array(dim_z, dim_z)
-        measurement noise matrix
-
-    Q : numpy.array(dim_x, dim_x)
-        process noise matrix
-
-    K : numpy.array
-        Kalman gain
-
-    y : numpy.array
-        innovation residual
-
-    x : numpy.array(dim_x)
-        predicted/updated state (result of predict()/update())
-
-    P : numpy.array(dim_x, dim_x)
-        predicted/updated covariance matrix (result of predict()/update())
-
-    log_likelihood : scalar
-        Log likelihood of last measurement update.
-
-    """
 
     def __init__(self, dim_x, dim_z, dt, hx, fx, points,
-                 sqrt_fn=None, x_mean_fn=None, z_mean_fn=None,
-                 residual_x=None,
-                 residual_z=None,
+                 x_mean_fn=None, z_mean_fn=None,
+                 residual_x=None, residual_z=None,
                  compute_log_likelihood=True):
-        """
-        Parameters
-        ----------
-
-        dim_x : int
-            Number of state variables for the filter. For example, if
-            you are tracking the position and velocity of an object in two
-            dimensions, dim_x would be 4.
-
-        dim_z : int
-            Number of of measurement inputs. For example, if the sensor
-            provides you with position in (x,y), dim_z would be 2.
-
-        dt : float
-            Time between steps in seconds.
-
-        hx : function(x)
-            Measurement function. Converts state vector x into a measurement
-            vector of shape (dim_z).
-
-        fx : function(x,dt)
-            function that returns the state x transformed by the
-            state transistion function. dt is the time step in seconds.
-
-        points : class
-            Class which computes the sigma points and weights for a UKF
-            algorithm. You can vary the UKF implementation by changing this
-            class. For example, MerweScaledSigmaPoints implements the alpha,
-            beta, kappa parameterization of Van der Merwe, and
-            JulierSigmaPoints implements Julier's original kappa
-            parameterization. See either of those for the required
-            signature of this class if you want to implement your own.
-
-        sqrt_fn : callable(ndarray), default = scipy.linalg.cholesky
-            Defines how we compute the square root of a matrix, which has
-            no unique answer. Cholesky is the default choice due to its
-            speed. Typically your alternative choice will be
-            scipy.linalg.sqrtm. Different choices affect how the sigma points
-            are arranged relative to the eigenvectors of the covariance matrix.
-            Usually this will not matter to you; if so the default cholesky()
-            yields maximal performance. As of van der Merwe's dissertation of
-            2004 [6] this was not a well reseached area so I have no advice
-            to give you.
-
-        x_mean_fn : callable  (sigma_points, weights), optional
-            Function that computes the mean of the provided sigma points
-            and weights. Use this if your state variable contains nonlinear
-            values such as angles which cannot be summed.
-
-        z_mean_fn : callable  (sigma_points, weights), optional
-            Same as x_mean_fn, except it is called for sigma points which
-            form the measurements after being passed through hx().
-
-        residual_x : callable (x, y), optional
-        residual_z : callable (x, y), optional
-            Function that computes the residual (difference) between x and y.
-            You will have to supply this if your state variable cannot support
-            subtraction, such as angles (359-1 degreees is 2, not 358). x and y
-            are state vectors, not scalars. One is for the state variable,
-            the other is for the measurement state.
-
-        compute_log_likelihood : bool (default = True)
-            Computes log likelihood by default, but this can be a slow
-            computation, so if you never use it you can turn this computation
-            off.
-
-        """
 
         self.Q = eye(dim_x)
         self.R = eye(dim_z)
@@ -202,19 +60,13 @@ class UnscentedKalmanFilter(object):
         self.compute_log_likelihood = compute_log_likelihood
         self.log_likelihood = math.log(sys.float_info.min)
 
-        if sqrt_fn is None:
-            self.msqrt = cholesky
-        else:
-            self.msqrt = sqrt_fn
-
-        # weights for the means and covariances.
+        self.msqrt = cholesky
         self.Wm, self.Wc = self.points_fn.weights()
 
         if residual_x is None:
             self.residual_x = np.subtract
         else:
             self.residual_x = residual_x
-
         if residual_z is None:
             self.residual_z = np.subtract
         else:
@@ -225,30 +77,6 @@ class UnscentedKalmanFilter(object):
 
 
     def predict(self, dt=None,  UT=None, fx_args=()):
-        r""" Performs the predict step of the UKF. On return, self.x and
-        self.P contain the predicted state (x) and covariance (P). '
-
-        Important: this MUST be called before update() is called for the first
-        time.
-
-        Parameters
-        ----------
-
-        dt : double, optional
-            If specified, the time step to be used for this prediction.
-            self._dt is used if this is not provided.
-
-        UT : function(sigmas, Wm, Wc, noise_cov), optional
-            Optional function to compute the unscented transform for the sigma
-            points passed through hx. Typically the default function will
-            work - you can use x_mean_fn and z_mean_fn to alter the behavior
-            of the unscented transform.
-
-        fx_args : tuple, optional, default (,)
-            optional arguments to be passed into fx() after the required state
-            variable.
-        """
-
         if dt is None:
             dt = self._dt
 
@@ -258,10 +86,7 @@ class UnscentedKalmanFilter(object):
         if UT is None:
             UT = unscented_transform
 
-        # calculate sigma points for given mean and covariance
         self.compute_process_sigmas(dt, *fx_args)
-
-        #and pass sigmas through the unscented transform
         self.x, self.P = UT(self.sigmas_f, self.Wm, self.Wc, self.Q,
                             self.x_mean, self.residual_x)
 
@@ -328,10 +153,6 @@ class UnscentedKalmanFilter(object):
 
 
     def cross_variance(self, x, z, sigmas_f, sigmas_h):
-        """
-        Compute cross variance of the state `x` and measurement `z`.
-        """
-
         Pxz = zeros((sigmas_f.shape[1], sigmas_h.shape[1]))
         N = sigmas_f.shape[0]
         for i in range(N):
@@ -342,191 +163,16 @@ class UnscentedKalmanFilter(object):
 
 
     def compute_process_sigmas(self, dt, *fx_args):
-        """
-        computes the values of sigmas_f. Normally a user would not call
-        this, but it is useful if you need to call update more than once
-        between calls to predict (to update for multiple simultaneous
-        measurements), so the sigmas correctly reflect the updated state
-        x, P.
-        """
-        # calculate sigma points for given mean and covariance
         sigmas = self.points_fn.sigma_points(self.x, self.P)
-
         for i, s in enumerate(sigmas):
             self.sigmas_f[i] = self.fx(s, dt, *fx_args)
 
 
     @property
     def likelihood(self):
-        """
-        likelihood of last measurment.
-
-        Computed from the log-likelihood. The log-likelihood can be very
-        small,  meaning a large negative value such as -28000. Taking the
-        exp() of that results in 0.0, which can break typical algorithms
-        which multiply by this value, so by default we always return a
-        number >= sys.float_info.min.
-
-        But really, this is a bad measure because of the scaling that is
-        involved - try to use log-likelihood in your equations!"""
-
         lh = math.exp(self.log_likelihood)
         if lh == 0:
              lh = sys.float_info.min
         return lh
 
-
-    def batch_filter(self, zs, Rs=None, UT=None):
-        """ Performs the UKF filter over the list of measurement in `zs`.
-
-        Parameters
-        ----------
-
-        zs : list-like
-            list of measurements at each time step `self._dt` Missing
-            measurements must be represented by 'None'.
-
-        Rs : list-like, optional
-            optional list of values to use for the measurement error
-            covariance; a value of None in any position will cause the filter
-            to use `self.R` for that time step.
-
-        UT : function(sigmas, Wm, Wc, noise_cov), optional
-            Optional function to compute the unscented transform for the sigma
-            points passed through hx. Typically the default function will
-            work - you can use x_mean_fn and z_mean_fn to alter the behavior
-            of the unscented transform.
-
-        Returns
-        -------
-
-        means: ndarray((n,dim_x,1))
-            array of the state for each time step after the update. Each entry
-            is an np.array. In other words `means[k,:]` is the state at step
-            `k`.
-
-        covariance: ndarray((n,dim_x,dim_x))
-            array of the covariances for each time step after the update.
-            In other words `covariance[k,:,:]` is the covariance at step `k`.
-        """
-
-        try:
-            z = zs[0]
-        except:
-            assert not isscalar(zs), 'zs must be list-like'
-
-        if self._dim_z == 1:
-            assert isscalar(z) or (z.ndim==1 and len(z) == 1), \
-            'zs must be a list of scalars or 1D, 1 element arrays'
-
-        else:
-            assert len(z) == self._dim_z, 'each element in zs must be a' \
-            '1D array of length {}'.format(self._dim_z)
-
-        z_n = np.size(zs, 0)
-        if Rs is None:
-            Rs = [None] * z_n
-
-        # mean estimates from Kalman Filter
-        if self.x.ndim == 1:
-            means = zeros((z_n, self._dim_x))
-        else:
-            means = zeros((z_n, self._dim_x, 1))
-
-        # state covariances from Kalman Filter
-        covariances = zeros((z_n, self._dim_x, self._dim_x))
-
-        for i, (z, r) in enumerate(zip(zs, Rs)):
-            self.predict(UT=UT)
-            self.update(z, r, UT=UT)
-            means[i,:]         = self.x
-            covariances[i,:,:] = self.P
-
-        return (means, covariances)
-
-
-    def rts_smoother(self, Xs, Ps, Qs=None, dt=None):
-        """ Runs the Rauch-Tung-Striebal Kalman smoother on a set of
-        means and covariances computed by the UKF. The usual input
-        would come from the output of `batch_filter()`.
-
-        Parameters
-        ----------
-
-        Xs : numpy.array
-           array of the means (state variable x) of the output of a Kalman
-           filter.
-
-        Ps : numpy.array
-            array of the covariances of the output of a kalman filter.
-
-        Qs: list-like collection of numpy.array, optional
-            Process noise of the Kalman filter at each time step. Optional,
-            if not provided the filter's self.Q will be used
-
-        dt : optional, float or array-like of float
-            If provided, specifies the time step of each step of the filter.
-            If float, then the same time step is used for all steps. If
-            an array, then each element k contains the time  at step k.
-            Units are seconds.
-
-        Returns
-        -------
-
-        x : numpy.ndarray
-           smoothed means
-
-        P : numpy.ndarray
-           smoothed state covariances
-
-        K : numpy.ndarray
-            smoother gain at each step
-
-        """
-
-        assert len(Xs) == len(Ps)
-        n, dim_x = Xs.shape
-
-        if dt is None:
-            dt = [self._dt] * n
-        elif isscalar(dt):
-            dt = [dt] * n
-
-        if Qs is None:
-            Qs = [self.Q] * n
-
-        # smoother gain
-        Ks = zeros((n, dim_x, dim_x))
-
-        num_sigmas = self._num_sigmas
-
-        xs, ps = Xs.copy(), Ps.copy()
-        sigmas_f = zeros((num_sigmas, dim_x))
-
-        for k in reversed(range(n-1)):
-            # create sigma points from state estimate, pass through state func
-            sigmas = self.points_fn.sigma_points(xs[k], ps[k])
-            for i in range(num_sigmas):
-                sigmas_f[i] = self.fx(sigmas[i], dt[k])
-
-            xb, Pb = unscented_transform(
-                    sigmas_f, self.Wm, self.Wc, self.Q,
-                    self.x_mean, self.residual_x)
-
-            # compute cross variance
-            Pxb = 0
-            for i in range(num_sigmas):
-                y = self.residual_x(sigmas_f[i], xb)
-                z = self.residual_x(sigmas[i], Xs[k])
-                Pxb += self.Wc[i] * outer(z, y)
-
-            # compute gain
-            K = dot(Pxb, inv(Pb))
-
-            # update the smoothed estimates
-            xs[k] += dot(K, self.residual_x(xs[k+1], xb))
-            ps[k] += dot(K, ps[k+1] - Pb).dot(K.T)
-            Ks[k] = K
-
-        return (xs, ps, Ks)
 
--- py/unscented.py
+++ py/unscented.py
@@ -1,4 +1,4 @@
-# unscented testing
+# -*- coding: utf-8 -*-
 
 
 from kalman.sigma_points import MerweScaledSigmaPoints
@@ -7,14 +7,13 @@ import numpy as np
 from numpy.random import randn
 
 
-def f_cv(x, dt):
-    """ state transition function for a 
-    constant velocity aircraft"""
-    return np.array([x[0]+0.7, x[1]+0.5, 0.7, 0.5])
-    
+def f_cv(x, dt, *args):
+	ctrl = args[0]
+	return np.array([x[0]+ctrl[0], x[1]+ctrl[1], x[2], x[3]])
+
 
 def h_cv(x):
-    return np.array([x[0], x[1]])
+	return np.array([x[0], x[1]])
 
 
 def run():
@@ -33,22 +32,22 @@ def run():
 					  [0.2, 1.0, 0.2, 0.2],
 					  [0.2, 0.2, 1.0, 0.2],
 					  [0.2, 0.2, 0.2, 1.0]])
-
+	
+	ctrl = np.array([xd,yd])
 	zs = [np.array([xi+i*xd,yi+i*yd]) for i in range(1,11)]
 	
-	print(ukf.x)
-	print(ukf.P)
-
-	return
-
+	#print(ukf.x)
+	#print(ukf.P)
+	
 	uxs = []
 	for z in zs:
-		ukf.predict()
+		ukf.predict(fx_args=(ctrl))
+		
+		print(ukf.sigmas_f)
+		return
+		
 		ukf.update(z)
 
-		print(ukf.x)
-		print(ukf.P)
-		print()
 
 	
 if __name__ == '__main__':
